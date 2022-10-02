2dbcfb33653ea29757c0526129f8c1a1ff478b5d
gkzhb <gkzhb98@gmail.com>
2022-10-02 15:54:33 +0800

    fix: process double quotes in new label string

--- rename-space.fish
+++ rename-space.fish
@@ -10,8 +10,10 @@ function rename_space
     # clear space label
     yabai -m space --label ''
   else
+    # avoid double quotes in new label, since they will casuse yabai output invalid JSON
+    set new_label (string replace -a \" \' $argv)
     # set new label
-    yabai -m space --label "$argv"
+    yabai -m space --label "$new_label"
   end
   # persist the space label info
   set csv_content (yabai -m query --spaces | jq -r '.[] | select(.label | length > 0) | [.index, .label] | @csv' | string split0)
